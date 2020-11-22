2ead975aa05dead08d93d3d941f60f9f5195c24b
Moti Elmakies <motiphone2003@gmail.com>
2020-11-23 00:25:17 +0200

    :sleeping: (end of day) in process of building useCallback practice page. good night :sparkling_heart:

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,17 @@ import UseEffectHook from './UseEffectHook';
 import UseMemoHook from './UseMemoHook';
 import UseRefHook from './UseRefHook';
 import UseContextHook from './UseContextHook';
+import UseCallbackHook from './UseCallbackHook';
 
 const App = () => {
   return (
     <div className='container'>
-      <UseContextHook />
+      {/*(<UseContextHook /> */}
       {/*<UseRefHook />*/}
       {/* <UseState /> */}
       {/* <UseEffectHook /> */}
       {/*<UseMemoHook />*/}
+      <UseCallbackHook />
     </div>
   );
 };
