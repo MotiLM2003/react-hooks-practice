65c1208b90c4c98d5c73c4cdf29e842902182ce7
Moti Elmakies <motiphone2003@gmail.com>
2020-11-22 14:57:16 +0200

    :champagne:  completed section - basic hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import '../App.css';
 
 const App = () => {
+  const [count, setCount] = useState(() => {
+    return 0;
+  });
+
+  const [settings, setSettings] = useState({ id: 1, name: 'moti' });
+
+  const decrement = () => {
+    setCount((prev) => prev - 1);
+  };
+
+  const increment = () => {
+    setCount((prev) => prev + 1);
+  };
   return (
-    <>
+    <div className='container'>
+      <button onClick={increment}>+</button>
+      <span>{count}</span>
+      <button onClick={decrement}>-</button>
+
+      <div>{JSON.stringify(settings, null)}</div>
       <div>
-        <h1>React Hooks</h1>
+        <input
+          type='text'
+          value={settings.name}
+          onChange={(e) => setSettings({ ...settings, name: e.target.value })}
+        />
       </div>
-    </>
+    </div>
   );
 };
 
